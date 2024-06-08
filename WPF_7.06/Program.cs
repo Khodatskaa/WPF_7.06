6b5ed87dbcf3e1a82df0b659f6402caf8dde04d2
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 20:16:09 +0300

    Ex 6 was done

--- WPF_7.06/Program.cs
+++ WPF_7.06/Program.cs
@@ -1,86 +1,65 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Threading;
 
 class Program
 {
-    static int[] numbers = new int[10000];
-    static int maxNumber;
-    static int minNumber;
-    static double averageNumber;
-    static object lockObj = new object();
-
     static void Main(string[] args)
     {
-        Random random = new Random();
-        for (int i = 0; i < numbers.Length; i++)
+        try
         {
-            numbers[i] = random.Next(1, 10001);
-        }
+            Console.WriteLine("Enter the beginning of the range:");
+            int startRange = int.Parse(Console.ReadLine());
 
-        Thread maxThread = new Thread(FindMax);
-        Thread minThread = new Thread(FindMin);
-        Thread avgThread = new Thread(FindAverage);
+            Console.WriteLine("Enter the end of the range:");
+            int endRange = int.Parse(Console.ReadLine());
 
-        maxThread.Start();
-        minThread.Start();
-        avgThread.Start();
+            Console.WriteLine("Enter the number of threads:");
+            int threadCount = int.Parse(Console.ReadLine());
 
-        maxThread.Join();
-        minThread.Join();
-        avgThread.Join();
-
-        WriteResultsToFile();
-
-        Console.WriteLine("All calculations are complete and results are written to the file.");
-    }
-
-    static void FindMax()
-    {
-        lock (lockObj)
-        {
-            maxNumber = numbers.Max();
-            Console.WriteLine("Maximum: " + maxNumber);
-        }
-    }
-
-    static void FindMin()
-    {
-        lock (lockObj)
-        {
-            minNumber = numbers.Min();
-            Console.WriteLine("Minimum: " + minNumber);
-        }
-    }
-
-    static void FindAverage()
-    {
-        lock (lockObj)
-        {
-            averageNumber = numbers.Average();
-            Console.WriteLine("Average: " + averageNumber);
-        }
-    }
-
-    static void WriteResultsToFile()
-    {
-        string filePath = "results.txt";
-        lock (lockObj)
-        {
-            using (StreamWriter writer = new StreamWriter(filePath))
+            if (startRange > endRange || threadCount <= 0)
             {
-                writer.WriteLine("Set of numbers:");
-                foreach (var number in numbers)
-                {
-                    writer.WriteLine(number);
-                }
-                writer.WriteLine("\nCalculation results:");
-                writer.WriteLine("Maximum: " + maxNumber);
-                writer.WriteLine("Minimum: " + minNumber);
-                writer.WriteLine("Average: " + averageNumber);
+                Console.WriteLine("Invalid input. Please enter valid range and thread count.");
+                return;
             }
+
+            int rangePerThread = (endRange - startRange + 1) / threadCount;
+            Thread[] threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                int start = startRange + i * rangePerThread;
+                int end = (i == threadCount - 1) ? endRange : start + rangePerThread - 1;
+
+                threads[i] = new Thread(() => PrintNumbers(start, end));
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("All threads completed.");
         }
-        Console.WriteLine("Results written to file: " + filePath);
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter valid integer values.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Input is too large or too small.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    static void PrintNumbers(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
