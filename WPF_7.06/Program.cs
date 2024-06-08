916c97090bcbc83eed5623509b4f11f8942b245a
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 19:49:10 +0300

    Ex 1 was done

--- WPF_7.06/Program.cs
+++ WPF_7.06/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace WPF_7._06
+﻿using System;
+using System.Threading;
+
+namespace WPF_7._06
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Thread thread = new Thread(new ThreadStart(DisplayNumbers));
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void DisplayNumbers()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
         }
     }
 }
