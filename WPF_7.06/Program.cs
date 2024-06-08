d635557ebfe2917aa7a385b0d4791bc2aa3d72ba
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 19:50:58 +0300

    Ex 2 was done

--- WPF_7.06/Program.cs
+++ WPF_7.06/Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace WPF_7._06
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the beginning of the range:");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the end of the range:");
+            int end = int.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(() => DisplayNumbers(start, end));
+            thread.Start();
+            Console.ReadLine(); 
+        }
+
+        static void DisplayNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100); 
+            }
         }
     }
 }
